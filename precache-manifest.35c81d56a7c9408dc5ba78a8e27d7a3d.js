88164cbd7bbbf8d6bfd908b0d28c03cea22e21b6
dheerajbajaj-200 <64356969+dheerajbajaj-200@users.noreply.github.com>
2020-04-26 20:00:48 +0530

    Updates

---
+++ precache-manifest.35c81d56a7c9408dc5ba78a8e27d7a3d.js
@@ -0,0 +1,190 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1ac25bdc7fbf68807bc1cf24fb45433",
+    "url": "/dheerajbajaj-200.github.io/whiteline.github.io/index.html"
+  },
+  {
+    "revision": "32eba4eb300d115c5edd",
+    "url": "/dheerajbajaj-200.github.io/whiteline.github.io/static/css/2.e691facb.chunk.css"
+  },
+  {
+    "revision": "52a3da72cfeae2eef239",
+    "url": "/dheerajbajaj-200.github.io/whiteline.github.io/static/css/main.7b83ac24.chunk.css"
+  },
+  {
+    "revision": "32eba4eb300d115c5edd",
+    "url": "/dheerajbajaj-200.github.io/whiteline.github.io/static/js/2.32c9c4f3.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/dheerajbajaj-200.github.io/whiteline.github.io/static/js/2.32c9c4f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52a3da72cfeae2eef239",
+    "url": "/dheerajbajaj-200.github.io/whiteline.github.io/static/js/main.5134ab0f.chunk.js"
+  },
+  {
+    "revision": "d27be454d712838dd7d6",
+    "url": "/dheerajbajaj-200.github.io/whiteline.github.io/static/js/runtime-main.816a5e52.js"
+  },
+  {
+    "revision": "c78b6b050204d2c6ebf4764392322051",
+    "url": "/dheerajbajaj-200.github.io/whiteline.github.io/static/media/apsara number pencil.c78b6b05.webp"
+  },
+  {
+    "revision": "f57a1f3078176dfd6378b8db610adf54",
+    "url": "/dheerajbajaj-200.github.io/whiteline.github.io/static/media/apsara pencil.f57a1f30.jpeg"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/dheerajbajaj-200.github.io/whiteline.github.io/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/dheerajbajaj-200.github.io/whiteline.github.io/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "0a166e71b97f4bbaa9b330d5054d4051",
+    "url": "/dheerajbajaj-200.github.io/whiteline.github.io/static/media/bags-2.0a166e71.jpg"
+  },
+  {
+    "revision": "c629783a7c2a7753d81772424f1bf256",
+    "url": "/dheerajbajaj-200.github.io/whiteline.github.io/static/media/bags.c629783a.jpg"
+  },
+  {
+    "revision": "793b90b69e270c196c9cdf89e24e0793",
+    "url": "/dheerajbajaj-200.github.io/whiteline.github.io/static/media/black-parker-pen---the-gift-artisans.793b90b6.jpg"
+  },
+  {
+    "revision": "55eeeeec54092b7c0adb2b559b028e77",
+    "url": "/dheerajbajaj-200.github.io/whiteline.github.io/static/media/bottles.55eeeeec.jpg"
+  },
+  {
+    "revision": "ec71651e041bb710ee12561c59481ee4",
+    "url": "/dheerajbajaj-200.github.io/whiteline.github.io/static/media/canvas.ec71651e.jpg"
+  },
+  {
+    "revision": "e7d25460d2a02c89d547ce35937269d0",
+    "url": "/dheerajbajaj-200.github.io/whiteline.github.io/static/media/cobra file.e7d25460.jpg"
+  },
+  {
+    "revision": "49cbb4cd0471d316e1c448834356df2c",
+    "url": "/dheerajbajaj-200.github.io/whiteline.github.io/static/media/customised pencil.49cbb4cd.jpg"
+  },
+  {
+    "revision": "0150037f84474979cf15b0e6a06c337a",
+    "url": "/dheerajbajaj-200.github.io/whiteline.github.io/static/media/customize pen.0150037f.jpg"
+  },
+  {
+    "revision": "f8719c4016df389489fd9454c77aacc8",
+    "url": "/dheerajbajaj-200.github.io/whiteline.github.io/static/media/customized t-shirt-2.f8719c40.jpg"
+  },
+  {
+    "revision": "ad9669f6abeeedf506cb19cfee317d8d",
+    "url": "/dheerajbajaj-200.github.io/whiteline.github.io/static/media/customized t-shirt.ad9669f6.jpg"
+  },
+  {
+    "revision": "ad9669f6abeeedf506cb19cfee317d8d",
+    "url": "/dheerajbajaj-200.github.io/whiteline.github.io/static/media/customized-t-shirt.ad9669f6.jpg"
+  },
+  {
+    "revision": "37e5aa7cd147804ee3e0c8b10c190490",
+    "url": "/dheerajbajaj-200.github.io/whiteline.github.io/static/media/emy bags.37e5aa7c.jpg"
+  },
+  {
+    "revision": "40238bc6074114c3c00b1bb541e3d180",
+    "url": "/dheerajbajaj-200.github.io/whiteline.github.io/static/media/excercise notbook.40238bc6.jpg"
+  },
+  {
+    "revision": "0cb5a5c0d251c109458c85c6afeffbaa",
+    "url": "/dheerajbajaj-200.github.io/whiteline.github.io/static/media/fa-brands-400.0cb5a5c0.svg"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/dheerajbajaj-200.github.io/whiteline.github.io/static/media/fa-brands-400.13685372.ttf"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/dheerajbajaj-200.github.io/whiteline.github.io/static/media/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "/dheerajbajaj-200.github.io/whiteline.github.io/static/media/fa-brands-400.c1868c95.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/dheerajbajaj-200.github.io/whiteline.github.io/static/media/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "/dheerajbajaj-200.github.io/whiteline.github.io/static/media/fa-regular-400.261d666b.eot"
+  },
+  {
+    "revision": "89ffa3aba80d30ee0a9371b25c968bbb",
+    "url": "/dheerajbajaj-200.github.io/whiteline.github.io/static/media/fa-regular-400.89ffa3ab.svg"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "/dheerajbajaj-200.github.io/whiteline.github.io/static/media/fa-regular-400.c20b5b73.woff2"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "/dheerajbajaj-200.github.io/whiteline.github.io/static/media/fa-regular-400.db78b935.ttf"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "/dheerajbajaj-200.github.io/whiteline.github.io/static/media/fa-regular-400.f89ea91e.woff"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/dheerajbajaj-200.github.io/whiteline.github.io/static/media/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/dheerajbajaj-200.github.io/whiteline.github.io/static/media/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/dheerajbajaj-200.github.io/whiteline.github.io/static/media/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/dheerajbajaj-200.github.io/whiteline.github.io/static/media/fa-solid-900.bea989e8.woff"
+  },
+  {
+    "revision": "ec763292e583294612f124c0b0def500",
+    "url": "/dheerajbajaj-200.github.io/whiteline.github.io/static/media/fa-solid-900.ec763292.svg"
+  },
+  {
+    "revision": "693a88c30a857d289f339130c5b76512",
+    "url": "/dheerajbajaj-200.github.io/whiteline.github.io/static/media/gift sets.693a88c3.jpg"
+  },
+  {
+    "revision": "7eca96fbf29ca4f689f539a5b12bce9a",
+    "url": "/dheerajbajaj-200.github.io/whiteline.github.io/static/media/index file.7eca96fb.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dheerajbajaj-200.github.io/whiteline.github.io/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2ecdb720bfa97c5b37984db38ba1fee8",
+    "url": "/dheerajbajaj-200.github.io/whiteline.github.io/static/media/parker set.2ecdb720.jpg"
+  },
+  {
+    "revision": "2d621463077a5aa92ef955fc4f29caeb",
+    "url": "/dheerajbajaj-200.github.io/whiteline.github.io/static/media/pencil.2d621463.jpg"
+  },
+  {
+    "revision": "2aa37b27d8913351e6c2270c271ef7b9",
+    "url": "/dheerajbajaj-200.github.io/whiteline.github.io/static/media/school notebooks-1.2aa37b27.jpg"
+  },
+  {
+    "revision": "6951dc267486ebd66c297f987fb74683",
+    "url": "/dheerajbajaj-200.github.io/whiteline.github.io/static/media/school notebooks.6951dc26.jpg"
+  },
+  {
+    "revision": "e51ced1b957f16d61b9448fcbbd6c71f",
+    "url": "/dheerajbajaj-200.github.io/whiteline.github.io/static/media/uniball pen.e51ced1b.jpg"
+  }
+]);
